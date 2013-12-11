234bf45e83e0f3fe2c0c262078b50ea91f5e1cd4
yzhao <yzhao@wgen.net>
2013-12-10 23:04:35 -0500

    some small changes

--- src/dice/Dice2.java
+++ src/dice/Dice2.java
@@ -17,10 +17,10 @@ public class Dice2 {
 	/**
 	 * Question 1:
 	 * Roll N: 
-	 * Write a function called rollN that takes a single integer parameter N and returns an array of N integers where 
-	 * the value of each integer is (randomly) between one and six. 
-	 * You should call a standard random number generator to produce the values between one and six - inclusive.
-	 * 
+	 * Write a function called rollN that takes a single integer parameter N and returns an array 
+	 * of N integers where the value of each integer is (randomly) between one and six. 
+	 * You should call a standard random number generator to produce the values between
+	 * one and six - inclusive.
 	 */
 	static int[] rollN(int n) {
 		int[] result = new int[n];
@@ -39,8 +39,8 @@ public class Dice2 {
 	 * What is the most frequent sum of five dice? 
 	 * Write a method called mostFrequentSum that produces the answer to this question. 
 	 * The function should take a single integer parameter and return an array of integers. 
-	 * In some cases, there might be a tie for the most frequent sum hence the need to return an array 
-	 * of integers. One of the test cases will call mostFrequentSum with 5 as a parameter.   
+	 * In some cases, there might be a tie for the most frequent sum hence the need to return an 
+	 * array of integers. One of the test cases will call mostFrequentSum with 5 as a parameter.   
 	 */
 	static int[] mostFrequentSum(int n) {
 		int[] result = {};
@@ -58,7 +58,6 @@ public class Dice2 {
 		int[] sumArr = new int[K];
 		for(int i = 0; i < K ; i++){
 			int indexSum = 0;
-			//do the sum
 			for(int j=0 ; j < n; j++){
 				indexSum += table.get(j)[i];
 			}
@@ -76,8 +75,7 @@ public class Dice2 {
 				countTable.put(sumArr[i], 1);
 			}
 		}
- 
-		return findMostFequentinTable1(countTable);
+		return findMostFequentinTable(countTable);
 	}
 	
 	/**
@@ -87,81 +85,46 @@ public class Dice2 {
 	 */
 	static int[] findMostFequentinTable(Hashtable<Integer, Integer> table){
 		//determine max value in the table
-		int max = 0;
-		for(Map.Entry<Integer, Integer> entry : table.entrySet()){
-			if(max < entry.getValue()){
-				max = entry.getValue();
-			}
-		}
-
-		ArrayList<Integer> resultList = new ArrayList<Integer>();
-		for(Map.Entry<Integer, Integer> entry : table.entrySet()){
-			if(entry.getValue() == max){
-				resultList.add(entry.getKey());
-			}
-		}
-		int [] result = new int[resultList.size()];
-		if(resultList.size()>1){
-			System.out.println(resultList.toString());
-		}
-		for(int i = 0; i < result.length; i++){
-			result[i] = resultList.get(i);
-		}
-		return result;
-	}
-	
-	static int[] findMostFequentinTable1(Hashtable<Integer, Integer> table){
-		//determine max value in the table
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		
-		Queue<Entry<Integer, Integer>> q = new PriorityQueue<Entry<Integer, Integer>>(
+		Queue<Entry<Integer, Integer>> maxQ = new PriorityQueue<Entry<Integer, Integer>>(
 				table.size(), new Comparator<Entry<Integer, Integer>>(){
-					
 			@Override
 			public int compare(Entry<Integer, Integer> o1,
 					Entry<Integer, Integer> o2) {
-				
-				if(o2.getValue() > o1.getValue()){
-					return 1;
-				}
-				if(o2.getValue() < o1.getValue()){
-					return -1;
-				}
-				return 0;
+				return o2.getValue() - o1.getValue();
 			}
 		});
 		
 		for(Entry<Integer, Integer> e : table.entrySet()){
-			q.add(e);
+			maxQ.add(e);
 		}
 		
-		int max = q.remove().getKey();
+		int max = maxQ.remove().getKey();
 		list.add(max);
-		while(!q.isEmpty() && max == q.peek().getKey()){
-			list.add(q.remove().getKey());
+		while(!maxQ.isEmpty() && max == maxQ.peek().getKey()){
+			list.add(maxQ.remove().getKey());
 		}
 		int[] result = new int[list.size()];
 		for(int i = 0; i < result.length; i++){
 			result[i] = list.get(i);
 		}
-		
 		return result;
 	}
 	
 	/**
 	 * Question 3:
 	 * One to one hundred
-	 * Write a function called one2oneHundred that takes as input an integer array of length 5 where 
-	 * each element is a number between 1 and 6 inclusive.	
+	 * Write a function called one2oneHundred that takes as input an integer array of length 5 
+	 * where each element is a number between 1 and 6 inclusive.	
 	 * Produce as output a random number in the range 1 to 100 inclusive.
      * If called a sufficiently large number of times the random numbers produced should be evenly 
-     * distributed between 1 and 100. You might not be able to get a very even distribution - but get 
-     * as close as you (reasonably) can.
-	 * Essentially generate a random number in the range 1 to 100 inclusive where the only source of 
-	 * randomness is the (simulated) roll of five dice
+     * distributed between 1 and 100. You might not be able to get a very even distribution
+     *  - but get as close as you (reasonably) can.
+	 * Essentially generate a random number in the range 1 to 100 inclusive where the only source 
+	 * of randomness is the (simulated) roll of five dice
 	 */
 	static int one2oneHundred(int[] dice) {
-	
+		//?? can I use built-in random function ?
 		int result = 0;
 	
 		return result;
